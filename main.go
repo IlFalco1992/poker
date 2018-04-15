03eb64d4d76170b9da5b9750c58ef47c68015588
root <root@testwebbizer.com>
2018-04-15 16:39:01 +0200

    server commit

--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"math/rand"
+	"strings"
 	"time"
 )
 
-func main()  {
+//Main
+func main() {
 	rand.Seed(time.Now().UnixNano())
 	values := strings.Split("2-3-4-5-6-7-8-9-10-J-Q-K-A", "-")
 	seeds := strings.Split("HDCS", "")
 	var deck Cards
-	for _, value := range values{
-		for _, seed := range seeds{
-			deck = append(deck, Card{value:Value(value), seed:seed})
+	for _, value := range values {
+		for _, seed := range seeds {
+			deck = append(deck, Card{value: Value(value), seed: seed})
 		}
 	}
 	rands := rand.Perm(len(deck))
-	for i, v := range rands{
+	for i, v := range rands {
 		deck[i], deck[v] = deck[v], deck[i]
 	}
 	var hand Hand
